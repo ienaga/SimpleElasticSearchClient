ec905ea2a281a9250fff456d57d18c6e99015579
thomas <thomas@sonicmoov.com>
2016-12-07 20:28:08 +0900

    debug and develop 1.0.0

--- src/ElasticSearch/Client.php
+++ src/ElasticSearch/Client.php
@@ -4,7 +4,7 @@ namespace SimpleElasticSearch;
 
 require_once __DIR__ . "/ClientInterface.php";
 require_once __DIR__ . "/Result.php";
-require_once __DIR__ . "/../../../simple-api-client/src/api/Client.php";
+//require_once __DIR__ . "/../../../simple-api-client/src/api/Client.php";
 
 use \SimpleApi\Client as BaseClient;
 
